def2ce09ae21e3f995d0416ccae61ff22e7e69f0
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-12-05 12:41:40 +0900

    video saving added

--- webstreaming.py
+++ webstreaming.py
@@ -5,10 +5,22 @@ import cv2
 import argparse
 import tflite_runtime.interpreter as tflite
 import numpy as np
+from img_proc.detector import ImageProcessor
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--ip',dest='ip', default='127.0.0.1')
+parser.add_argument('-p','--port', type=int,dest='port')
+parser.add_argument('-v','--viddir', type=str, dest='vid_dir')
+args = parser.parse_args()
+
+FRAME_RES = (640,480)
+model_path = 'savedmodels/mobv3_small_head.tflite'
+imgproc = ImageProcessor(FRAME_RES, args.vid_dir, model_path).start()
+
 # FRAME_RES = (640,480)
 # FRAME_RES_HW = (FRAME_RES[1],FRAME_RES[0])
 
-vs = VideoStream(usePiCamera=True, resolution=FRAME_RES,framerate=10).start()
+# vs = VideoStream(usePiCamera=True, resolution=FRAME_RES,framerate=10).start()
 
 app = Flask(__name__)
 
@@ -31,24 +43,27 @@ def index():
     return render_template('index.html')
 
 def generator():
-    global vs, interpreter
+    global imgproc
     while True:
-        frame = vs.read().copy()
-        resized_frame = cv2.resize(frame, dsize=input_size_wh)
-        interpreter.set_tensor(input_idx,resized_frame[np.newaxis,...])
-        interpreter.invoke()
-        heatmap = np.squeeze(interpreter.get_tensor(output_idx))
-        pos = np.unravel_index(heatmap.flatten().argmax(),output_size_hw)
-        pos = np.multiply(pos, resize_ratio).astype(np.int)
-        r, c = pos
-        r_min = max(r-5,0)
-        r_max = r+5
-        c_min = max(c-5,0)
-        c_max = c+5
-        frame[r_min:r_max,c_min:c_max] = [0,255,0]
-        flag, encoded_image = cv2.imencode('.jpg',frame)
-        if not flag:
-            continue
+        # frame = vs.read().copy()
+        # resized_frame = cv2.resize(frame, dsize=input_size_wh)
+        # interpreter.set_tensor(input_idx,resized_frame[np.newaxis,...])
+        # interpreter.invoke()
+        # heatmap = np.squeeze(interpreter.get_tensor(output_idx))
+        # pos = np.unravel_index(heatmap.flatten().argmax(),output_size_hw)
+        # pos = np.multiply(pos, resize_ratio).astype(np.int)
+        # r, c = pos
+        # r_min = max(r-5,0)
+        # r_max = r+5
+        # c_min = max(c-5,0)
+        # c_max = c+5
+        # frame[r_min:r_max,c_min:c_max] = [0,255,0]
+        if imgproc.is_updated():
+            frame = imgproc.read()
+            flag, encoded_image = cv2.imencode('.jpg',frame)
+            if not flag:
+                continue
+        else: continue
         
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n'+
@@ -60,12 +75,7 @@ def video_feed():
     return Response(generator(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i','--ip',dest='ip', default='127.0.0.1')
-    parser.add_argument('-p','--port', type=int,dest='port')
-    args = parser.parse_args()
 
-    app.run(host=args.ip, port=args.port, debug=True, threaded=True, use_reloader=False)
+app.run(host=args.ip, port=args.port, debug=True, threaded=True, use_reloader=False)
 
-vs.stop()
+imgproc.stop()
