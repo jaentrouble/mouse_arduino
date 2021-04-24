48274316dcb931cff5b6c1b139c8748b365db31e
jaentrouble <jaentrouble@gmail.com>
2021-04-24 17:45:17 +0900

    new model

--- webstreaming.py
+++ webstreaming.py
@@ -15,7 +15,7 @@ parser.add_argument('-nt','--notracking', default=False, action='store_true', de
 args = parser.parse_args()
 
 FRAME_RES = (640,480)
-model_path = 'savedmodels/mobv3_small_07_head_q3q4_quan.tflite'
+model_path = 'savedmodels/mobv3_small_07_head_4cham_0q0_quan.tflite'
 imgproc = ImageProcessor(
     FRAME_RES, args.vid_dir, model_path, record_tracking=(not args.nt)
 ).start()
